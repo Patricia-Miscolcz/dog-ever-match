e81b258f2fc7810307ce652b49188cf0f569430c
Patricia-Miscolcz <patriciaampese@hotmail.com>
2024-08-15 21:01:58 -0300

    wip

---
+++ app/(app)/inputScreen.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+
+export default function InputScreenApp (){
+    return(
+
+        <>
+        <Stack.Screen/>
+        <InputScreen/>
+        
+        </>
+    )
+}
